b204c0e0ff3808782dfcc878de9061859656f3fd
Rubber12Ducky19 <stoltzjason@yahoo.com>
2025-09-03 17:02:20 -0700

    CH2L2

---
+++ src/main.py
@@ -0,0 +1,10 @@
+#./main.sh
+from textnode import TextNode
+from textnode import TextType
+
+def main():
+    test = TextNode("This is a test", TextType.TEXT,)
+    print (test)
+
+print ("hello world")
+main()
